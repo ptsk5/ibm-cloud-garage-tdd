2b044f1a43baedafaa37786f2cbd3fe382a7c264
Jiri Petnik <jiri_petnik@cz.ibm.com>
2020-02-19 15:55:54 +0100

    introduce guard clause to get to green bar

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -12,9 +12,14 @@ describe('the palindrome canary spec', () => {
   it('false for dad mom', () => {
     expect(isPalindrome('dad mom')).toBe(false);
   });
-  it.todo ('true for whitespace');
+  it('false for whitespace',() => {
+    expect(isPalindrome('    ')).toBe(false); 
+  });
   it.todo ('error for empty string');
   it.todo ('error for not a string');
 });
 
-let isPalindrome = (phrase) => phrase == phrase.split('').reverse().join(''); 
+let isPalindrome = phrase => {
+  if (phrase.trim() === '') return false;
+  return phrase === phrase.split('').reverse().join('');
+}; 
