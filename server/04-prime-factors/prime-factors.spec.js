239df2be2d6d0d77b6a3a4b099cb354ed1ca3ec8
Jiri Petnik <jiri_petnik@cz.ibm.com>
2020-02-23 14:08:37 +0100

    prime factors for 4

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -14,7 +14,9 @@ describe('a prime factors function should', () => {
   it('return 3 for 3', () => {
     expect(findPrimeFactors(3)).toEqual([3]);
   });
-  it.todo('return 2, 2 for 4');
+  it('return 2, 2 for 4', () => {
+    expect(findPrimeFactors(4)).toEqual([2,2]);
+  });
   it.todo('return 5 for 5');
   it.todo('return 2, 3 for 6');
   it.todo('return 7 for 7');
@@ -23,6 +25,13 @@ describe('a prime factors function should', () => {
 });
 
 let findPrimeFactors = (number) => { 
-  if (number === 1) return [];
-  return [number]; 
+  let factors = [];
+  if (number > 1) {
+    if (number % 2 === 0) {
+      factors.push(2);
+        number /= 2;
+    }
+    if (number > 1) factors.push(number);
+  }
+  return factors;
 };
